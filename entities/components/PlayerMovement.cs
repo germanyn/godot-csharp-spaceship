f1dfed1b931d6c5ec760a33754e4e72c768fe7c2
Germano Gascho <germanogascho@gmail.com>
2026-04-28 12:27:44 -0300

    first commit

---
+++ entities/components/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+namespace SpaceAce.entities.components;
+
+public partial class PlayerMovement : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
